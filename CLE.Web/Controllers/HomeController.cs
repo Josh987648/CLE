11ca98e32b7d4b9636f620af91fa89aafc2c578d
Joshua Rodriguez <joshua.rodriguez3049@gmail.com>
2017-07-18 11:27:53 -0400

    Change wrong controller

--- CLE.Web/Controllers/HomeController.cs
+++ CLE.Web/Controllers/HomeController.cs
@@ -4,36 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Mvc;
 
 namespace CLE.Web.Controllers
 {
-    public class HomeController : ApiController
+    public class HomeController : Controller
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+                
+        public ActionResult Index()
         {
-            return new string[] { "value1", "value2" };
+            return View();
         }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
